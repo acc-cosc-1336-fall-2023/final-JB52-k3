6d85b8a5358d5a7f62350f7f60a5232d4f7010ba
JB <150109296+JB52-k3@users.noreply.github.com>
2024-12-11 03:18:22 +0000

    question a

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def create_multiplication_table():
+    list0 = []
+    for i in range(5):
+        row_list = []
+        for j in range(10):
+            row_list.append((i + 1) * (j + 1))
+        list0.append(row_list)
+    return list0
+
+def display_multiplication_table():
+    list0 = create_multiplication_table()
+    for row_list in list0:
+        for item in row_list:
+            print(str(item).rjust(3, " "), end = " ")
+
+        print(" ")
